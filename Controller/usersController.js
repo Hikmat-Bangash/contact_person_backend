074f7f4c4bb53abeb19c38ecdaa3d8a77faf509f
Hikmat-Bangash <100120213+Hikmat-Bangash@users.noreply.github.com>
2022-10-09 00:10:05 +0500

    Update usersController.js

--- Controller/usersController.js
+++ Controller/usersController.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2";
 //----------- MYSQL CONNECTION ---------
 const db = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
+    host: "localhost",
+    user: "root",
     password: "",
-    database: process.env.DATABASE
+    database: "node_express_crud"
 })
 // connection end
 
